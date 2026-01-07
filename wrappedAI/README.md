81582573cd74c0d777d7027d9f2c5d90cb05882c
kelp-Shake <affororachel@gmail.com>
2026-01-07 13:42:44 -0500

    msgs finished and bolierplate of mvp completed

--- wrappedAI/README.md
+++ wrappedAI/README.md
@@ -1,8 +1,6 @@
+# Todo: will complete later
 ## Extension Setup
-### On the wrappedAI branch
-+ switch to the branch and install and build npm 
-  + `npm install`
-  + `npm run build`
++ Load the wrappedAi folder 
 ---
 ## Testing the extension on chrome 
 + Load the extension from `/wrappedAI`
--- wrappedAI/popup/src/Wrapped.jsx
+++ wrappedAI/popup/src/Wrapped.jsx
@@ -1,25 +1,42 @@
-import { useState } from 'react'
 import logo from '/images/logo.png'
-function Wrapped() {
-    const [tracking, changeBut] = useState(false);
-    return (
-        <div className="extension">
-            <header>
-                <h1>wrapped AI</h1>
-                <img src={logo} alt="logo" />
-            </header>
-            <div className="wrapped-button">
-                <h3>Download Wrapped</h3>
-            </div>
-            <div className="track-button">
-                <button
-                    onClick={() => changeBut(!tracking)}
-                    className={tracking ? 'stop' : 'start'}>
-                    {tracking ? "Stop Tracking" : "Start Tracking"}
-                </button>
+import { useState } from 'react'
 
-            </div>
+function Wrapped() {
+    const [isTracking, setTracking] = useState(false);
+    function updateTracking() {
+        const changeTracking = !isTracking;
+        chrome.runtime.sendMessage({ type: "SET_TRACKING", tracking: changeTracking });
+        setTracking(changeTracking);
+    }
+    // TODO  downloadWrapped
+    //Only for MVP: send a message to background to start downloading wrapped
+    /* html:
+        <div className="wrapped-button">
+        <button>onClick ={downloadWrapped()} </button>
         </div>
-    );
+    */
+    function downloadWrapped() {
+        chrome.runtime.sendMessage({ type: "DOWNLOAD" });
+    }
+    // Final: visit the wrapped site so link the ai wrapped site no message 
+
+    return (<div className="extension">
+        <header>
+            <h1>AI Wrapped</h1>
+            <img src={logo} alt="logo" />
+        </header>
+        <div className="wrapped-button">
+            <h3>Download Wrapped</h3>
+
+        </div>
+        <div className="track-button">
+            <button
+                onClick={updateTracking}
+                className={isTracking ? 'stop' : 'start'}>
+                {isTracking ? "Stop Tracking" : "Start Tracking"}
+            </button>
+
+        </div>
+    </div>);
 }
 export default Wrapped;
